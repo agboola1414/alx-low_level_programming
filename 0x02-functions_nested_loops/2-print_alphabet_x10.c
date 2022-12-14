a0589689991823471093cd59f0f15628b2f718a2
agboola1414 <agboolaolawalesept@gmail.com>
2022-12-13 19:03:55 -0800

    ten times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,12 +6,12 @@
 
 void print_alphabet_x10(void)
 {
-	int count = 0;
+	int 10;
 	char letter;
-
-	while (count++ <= 9)
+	
+	for (10 = 0; 10 <= 9; ten++)
 	{
-	for (letter = 'a'; letter <= 'z'; letter++)
-		_putchar(letter);
-	_putchar('\n');
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+		_putchar('\n');
 	}
