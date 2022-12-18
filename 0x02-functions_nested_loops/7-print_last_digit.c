aa8b7b420555d3732972721ec20b134fea319c02
agboola1414 <agboolaolawalesept@gmail.com>
2022-12-17 21:47:59 -0800

    print_last_digit.

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * print_last_digit - print the last digit
  * @n: the number
@@ -6,10 +7,11 @@
  */
 int print_last_digit(int n)
 {
-	int ld = n % d;
+	int ld = n % 10;
 
 	if (ld < 0)
 		ld *= -1;
+
 	_putchar(ld + '0');
 
 	return (0);
