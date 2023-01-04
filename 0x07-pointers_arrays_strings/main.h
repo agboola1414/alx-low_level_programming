da2df723c8ef88a99ccace69730623b8a2e7ca32
agboola1414 <agboolaolawalesept@gmail.com>
2023-01-03 21:21:37 -0800

    memset

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,12 +1,11 @@
-fndef MAIN_H_INCLUDED
-
-#define MAIN_H_INCLUDED
+#ifndef MAIN_H
+#define MAIN_H
 
 /**
  *  * void prototypes()
  *
  *   * int prototypes()
- *    */
+ */
 
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
@@ -17,4 +16,5 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
+
 #endif /* MAIN_H_INCLUDED */
