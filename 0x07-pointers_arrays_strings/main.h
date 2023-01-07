1a0148434850ef04b3e7c5328669d4d04e40d77a
agboola1414 <agboolaolawalesept@gmail.com>
2023-01-07 09:28:12 -0800

    memset task 0

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,12 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-
-/**
- *  * void prototypes()
- *
- *   * int prototypes()
- */
-
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
@@ -17,4 +10,4 @@ void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
 
-#endif /* MAIN_H_INCLUDED */
+#endif
