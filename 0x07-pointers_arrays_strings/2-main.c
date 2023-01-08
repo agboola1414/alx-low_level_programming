3083f379ca2b168b3b9b0c4c47f50a5be32649d4
agboola1414 <agboolaolawalesept@gmail.com>
2023-01-08 06:06:45 -0800

    strchr task 3

--- 0x07-pointers_arrays_strings/2-main.c
+++ 0x07-pointers_arrays_strings/2-main.c
@@ -1,18 +1,10 @@
 #include "main.h"
-
 #include <stdio.h>
 
-
-
 /**
- *
- *  * main - check the code
- *
- *   *
- *
- *    * Return: Always 0.
- *
- *     */
+ * main - check the code
+ * Return: Always 0.
+ */
 
 int main(void)
 
