f8509fbe8679cfed009574b19afd95f666f4a99f
agboola1414 <agboolaolawalesept@gmail.com>
2023-01-10 00:01:03 -0800

    1 print rev recursion task 2

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,39 +1,20 @@
-/*
- *
- *                                                                                                                                                                       * file: 1-print_rev_recursion.c
- *
- *                                                                                                                                                                        * Auth: sum
- *
- *                                                                                                                                                                         */
-
-
-
 #include "main.h"
 
-
-
 /**
- *
- *  * _print_rev_recursion - print a string in reverse.
- *
- *   * @s: The string to be printed.
- *
- *    */
-
-
+ * _print_rev_recursion - Print a string in reverse.
+ * @s: The string to be printed.
+ * Return: no return
+ */
 
 void _print_rev_recursion(char *s)
 
 {
+	if (*s)
 
-	        if (*s)
+	{
+	_print_rev_recursion(s + 1);
 
-			        {
+	_putchar(*s);
 
-					                _print_rev_recursion(s +1);
-
-							                _putchar(*s);
-
-									        }
-
-
+	}
+}
